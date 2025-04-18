a44bffa8467c3e60aeba645eb71e0269494d748b
Dave Davis <dave@davemdavis.net>
2025-04-18 14:33:53 -0400

    Add mcp.json configuration and refactor MonkeyTools to instantiate MonkeyService locally

--- src/Monkey/Tools/MonkeyTools.cs
+++ src/Monkey/Tools/MonkeyTools.cs
@@ -11,16 +11,18 @@ namespace Monkey
     public static class MonkeyTools
     {
         [Function("GetMonkeys")]
-        public static async Task<string> GetMonkeys([McpToolTrigger(GetMonkeysToolName, GetMonkeysToolDescription)] ToolInvocationContext context, MonkeyService monkeyService)
+        public static async Task<string> GetMonkeys([McpToolTrigger(GetMonkeysToolName, GetMonkeysToolDescription)] ToolInvocationContext context)
         {
+            var monkeyService = new MonkeyService();
             var monkeys = await monkeyService.GetMonkeys();
             return JsonSerializer.Serialize(monkeys);
         }
         
         [Function("GetMonkey")]
-        public static async Task<string> GetMonkey([McpToolTrigger(GetMonkeyToolName, GetMonkeyToolDescription)] ToolInvocationContext context, MonkeyService monkeyService, 
+        public static async Task<string> GetMonkey([McpToolTrigger(GetMonkeyToolName, GetMonkeyToolDescription)] ToolInvocationContext context,  
              [McpToolProperty(MonkeyNamePropertyName, PropertyType, MonketNamePropertyDescription )] string name)
         {
+            var monkeyService = new MonkeyService();
             var monkey = await monkeyService.GetMonkey(name);
             return JsonSerializer.Serialize(monkey);
         }                
